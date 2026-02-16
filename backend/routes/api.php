3e3ed41fb8e5464159813357f8c2cf59f0311780
orhideaqadraku <orhideaaq@gmail.com>
2026-02-16 20:09:52 +0100

    first commit

---
+++ backend/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'OK',
+        'message' => 'API is working'
+    ]);
+});
